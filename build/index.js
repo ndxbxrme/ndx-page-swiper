328e27171594a11423aa9aff0860996b384e0b27
ndxbxrme <lewis_the_cat@hotmail.com>
2018-02-17 22:31:31 +0000

    ios friendly

--- build/index.js
+++ build/index.js
@@ -11,38 +11,77 @@
     module = angular.module('ndx', []);
   }
 
-  module.directive('swiper', function($timeout) {
+  module.factory('SwiperPage', function() {
+    var page;
+    page = 0;
+    return {
+      getPage: function() {
+        return page;
+      },
+      setPage: function(_page) {
+        return page = _page;
+      }
+    };
+  });
+
+  module.directive('swiper', function($timeout, SwiperPage, $state, $window) {
     return {
       restrict: 'AE',
+      transclude: true,
+      template: '<div class="ndx-swiper-inner"><ng-transclude></ng-transclude></div>',
       scope: {
         page: '=?',
         nopages: '=?',
         change: '=?'
       },
       link: function(scope, elem, attrs) {
-        var hammerSwiper, name, setPage, swiper, x;
+        var bodySelector, hammerSwiper, name, setPage, swiper, x;
+        bodySelector = 'html, body';
+        if ($window.navigator.userAgent.match(/(iPod|iPhone|iPad|Android)/)) {
+          bodySelector = 'body';
+        }
         name = attrs.name;
         scope.page = scope.page || 0;
-        setPage = function(page) {
+        setPage = function(page, immediate) {
+          //$timeout ->
+          if (page < 0) {
+            page = 0;
+          }
+          if (page > scope.nopages - 1) {
+            page = scope.nopages - 1;
+          }
+          if (!immediate) {
+            swiper.addClass('animate').removeClass('dragging');
+          } else {
+            swiper.removeClass('animate').removeClass('dragging');
+          }
+          swiper.css({
+            transform: 'translate3d(' + -100 * page + '%, 0, 0)'
+          });
           return $timeout(function() {
-            var oldPage;
-            oldPage = scope.page;
+            var i, len, newstate, oldPage, ref, state;
+            oldPage = SwiperPage.getPage();
             scope.page = page;
-            if (scope.page < 0) {
-              scope.page = 0;
-            }
-            if (scope.page > scope.nopages - 1) {
-              scope.page = scope.nopages - 1;
-            }
-            swiper.addClass('animate').removeClass('dragging').css({
-              transform: 'translate3d(' + -100 * scope.page + '%, 0, 0)'
-            });
             if (oldPage !== scope.page) {
+              newstate = null;
+              ref = $state.get();
+              for (i = 0, len = ref.length; i < len; i++) {
+                state = ref[i];
+                if (state.params && state.params.page === page) {
+                  newstate = state;
+                }
+              }
+              if (newstate && newstate.name !== $state.current.name) {
+                $timeout(function() {
+                  return $state.go(newstate.name);
+                }, 500);
+              }
+              SwiperPage.setPage(scope.page);
               if (typeof scope.change === "function") {
                 scope.change(scope.page);
               }
-              if ($('html, body').scrollTop()) {
-                return $('html, body').animate({
+              if ($(bodySelector).scrollTop()) {
+                return $(bodySelector).animate({
                   scrollTop: 0
                 }, 400);
               }
@@ -53,18 +92,26 @@
           scope.$parent[name] = {
             goToPage: function(page) {
               return setPage(page);
+            },
+            goImmediate: function(page) {
+              return setPage(page, true);
             }
           };
         }
-        swiper = $(elem[0]);
-        scope.nopages = scope.nopages || swiper.children().length;
-        swiper.addClass('ndx-swiper').css({
+        $(elem[0]).addClass('ndx-swiper');
+        swiper = $('.ndx-swiper-inner', elem[0]);
+        $timeout(function() {
+          scope.nopages = scope.nopages || $('ng-transclude', swiper).children().length;
+          return console.log(swiper.children().length);
+        });
+        swiper.css({
           transform: 'translate3d(' + -100 * scope.page + '%, 0, 0)'
         });
+        setPage(SwiperPage.getPage(), true);
         hammerSwiper = new Hammer(elem[0]);
         hammerSwiper.get('pan').set({
           direction: Hammer.DIRECTION_HORIZONTAL,
-          threshold: 100
+          threshold: 60
         });
         x = 0;
         return hammerSwiper.on('panstart', function() {
--- src/index.coffee
+++ src/index.coffee
@@ -4,49 +4,82 @@ try
   module = angular.module 'ndx'
 catch e
   module = angular.module 'ndx', []
-module.directive 'swiper', ($timeout) ->
+module.factory 'SwiperPage', ->
+  page = 0
+  getPage: ->
+    page
+  setPage: (_page) ->
+    page = _page
+module.directive 'swiper', ($timeout, SwiperPage, $state, $window) ->
   restrict: 'AE'
+  transclude: true
+  template: '<div class="ndx-swiper-inner"><ng-transclude></ng-transclude></div>'
   scope:
     page: '=?'
     nopages: '=?'
     change: '=?'
   link: (scope, elem, attrs) ->
+    bodySelector = 'html, body'
+    if $window.navigator.userAgent.match /(iPod|iPhone|iPad|Android)/
+      bodySelector = 'body'
     name = attrs.name
     scope.page = scope.page or 0
-    setPage = (page) ->
-      $timeout ->
-        oldPage = scope.page
-        scope.page = page
-        if scope.page < 0
-          scope.page = 0
-        if scope.page > scope.nopages - 1
-          scope.page = scope.nopages - 1
+    setPage = (page, immediate) ->
+      #$timeout ->
+      if page < 0
+        page = 0
+      if page > scope.nopages - 1
+        page = scope.nopages - 1
+      if not immediate
         swiper
         .addClass 'animate'
         .removeClass 'dragging'
-        .css
-          transform: 'translate3d(' + -100 * scope.page + '%, 0, 0)'
+      else
+        swiper
+        .removeClass 'animate'
+        .removeClass 'dragging'
+      swiper
+      .css
+        transform: 'translate3d(' + -100 * page + '%, 0, 0)'
+      $timeout ->
+        oldPage = SwiperPage.getPage()
+        scope.page = page
         if oldPage isnt scope.page
+          newstate = null
+          for state in $state.get()
+            if state.params and state.params.page is page
+              newstate = state
+          if newstate and newstate.name isnt $state.current.name
+            $timeout ->
+              $state.go newstate.name
+            , 500
+          SwiperPage.setPage scope.page
           scope.change? scope.page
-          if $('html, body').scrollTop()
-            $('html, body').animate
+          if $(bodySelector).scrollTop()
+            $(bodySelector).animate
               scrollTop: 0
             , 400
     if name
       scope.$parent[name] =
         goToPage: (page) ->
           setPage page
-    swiper = $ elem[0]
-    scope.nopages = scope.nopages or swiper.children().length
-    swiper
+        goImmediate: (page) ->
+          setPage page, true
+    $ elem[0]
     .addClass 'ndx-swiper'
+    swiper = $ '.ndx-swiper-inner', elem[0]
+    $timeout ->
+      scope.nopages = scope.nopages or $('ng-transclude', swiper).children().length
+      console.log swiper.children().length
+    swiper
     .css
       transform: 'translate3d(' + -100 * scope.page + '%, 0, 0)'
+    setPage SwiperPage.getPage(), true
     hammerSwiper = new Hammer elem[0]
     hammerSwiper.get 'pan'
     .set
       direction: Hammer.DIRECTION_HORIZONTAL
-      threshold: 100
+      threshold: 60
     x = 0
     hammerSwiper
     .on 'panstart', ->
